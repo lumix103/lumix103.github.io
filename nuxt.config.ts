0ff4ef5250bbe5f8bd1070773a188c4bff551ff2
Fabs <15038234+lumix103@users.noreply.github.com>
2024-06-27 17:58:10 -0700

    Added tailwind module in nuxtConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   postcss: {
     plugins: {
@@ -13,10 +13,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       bodyAttrs: {
-        class: 'bg-mirage-950',
+        class: "bg-mirage-950",
       },
     },
   },
 
-  modules: ["@nuxt/icon"]
-})
+  modules: ["@nuxt/icon", "@nuxtjs/tailwindcss"],
+});
